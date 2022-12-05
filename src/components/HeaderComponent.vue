e2a5c6e06cee4879752490cbadfc6ba38161aaa2
MicheleRai <rainonemichele1999@gmail.com>
2022-12-05 18:44:18 +0100

    nav bar added

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -7,12 +7,33 @@
           alt="logo"
         >
         <div class="text">
-          <p
+          <a
             v-for="part in navParts"
             :key="part"
+            href="!"
           >
-            {{ part }}
-          </p>
+            {{ part }} <font-awesome-icon
+              class="icona"
+              icon="fa-solid fa-chevron-down"
+            />
+          </a>
+          <a
+            href="!"
+            class="carrello"
+          >
+            <font-awesome-icon
+              class="icona-carrello"
+              icon="fa-solid fa-cart-shopping"
+            />
+            <div
+              class="contenuto-carrello"
+              v-bind="carrello"
+            >{{ carrello }}</div>
+          </a>
+          <a href="!">
+            <font-awesome-icon icon="fa-solid fa-circle-user" />
+            .
+          </a>
         </div>
       </nav>
     </div>
@@ -25,6 +46,7 @@ export default {
   data() {
     return {
       navParts: ['Home', 'Pages', 'Courses', 'Features', 'Blog', 'Shop'],
+      carrello: 0,
     };
   },
 };
@@ -35,13 +57,42 @@ export default {
     background-color: #E9E6E3;
     nav{
       display: flex;
-      justify-content: space-between;
+      justify-content: space-around;
       align-items: center;
+      height: 70px;
+      img{
+        height: 30px;
+      }
       .text{
         display: flex;
         justify-content: space-between;
         align-items: center;
         gap: 2rem;
+        a{
+          text-decoration: none;
+          color: #3F3A64;
+          .icona{
+            margin-left: .2rem;
+            font-size: 15px;
+          }
+        }
+        .carrello{
+          position: relative;
+          .icona-carrello{
+            font-size: 20px;
+          }
+          .contenuto-carrello{
+            background-color: green;
+            color: white;
+            border-radius: 100%;
+            text-align: center;
+            position: absolute;
+            padding: .2rem .4rem;
+            font-size: 12px;
+            top: -15px;
+            right: -12px;
+          }
+        }
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
-  faChevronDown, faDownload, faCircle, faSquareFacebook,
+  faChevronDown, faDownload, faCircle, faCopyright,
   faCartShopping, faCircleUser, faMagnifyingGlass, faCirclePlus,
   faCircleMinus, faFileLines, faUser, faArrowRight,
-  faCheck, faLocationDot, faTwitter, faInstagram,
-  faLinkedin, faCopyright,
+  faCheck, faLocationDot,
 } from '@fortawesome/free-solid-svg-icons';
+import {
+  faTwitter, faInstagram, faLinkedin, faSquareFacebook,
+} from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 
