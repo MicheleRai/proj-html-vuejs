0fbed207724be3610748603027a399d5e95fe4c8
MicheleRai <rainonemichele1999@gmail.com>
2022-12-06 22:28:54 +0100

    base tiket added

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -99,25 +99,153 @@
               alt="corso"
               class="poster"
             >
-            <p>{{ objCorso.price }}</p>
-            <p>
-              <img
-                :src="objCorso.teacherPhoto"
-                alt="teacher-photo"
-              >
-              <span>{{ objCorso.teacher }}</span>
+            <p class="price">
+              <strong>{{ objCorso.price }}</strong>
             </p>
-            <h2>{{ objCorso.courses }}</h2>
-            <p>
-              <span>
-                <font-awesome-icon icon="fa-solid fa-file-lines" />
-                {{ objCorso.lessons }} Lessons
-              </span>
-              <span>
-                <font-awesome-icon icon="fa-solid fa-user" />
-                {{ objCorso.students }} Students
-              </span>
+            <div class="card-text">
+              <p class="teacher-container">
+                <img
+                  :src="objCorso.teacherPhoto"
+                  alt="teacher-photo"
+                >
+                <span>{{ objCorso.teacher }}</span>
+              </p>
+              <h2>{{ objCorso.courses }}</h2>
+              <p class="contenuto">
+                <span class="gray">
+                  <font-awesome-icon icon="fa-solid fa-file-lines" />
+                  {{ objCorso.lessons }} Lessons
+                </span>
+                <span class="gray right">
+                  <font-awesome-icon icon="fa-solid fa-user" />
+                  {{ objCorso.students }} Students
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="button-container">
+          <button>View all courses <font-awesome-icon icon="fa-solid fa-arrow-right" /></button>
+        </div>
+      </div>
+    </div>
+    <div class="fourth-section">
+      <div class="first-container">
+        <div class="text">
+          <p
+            class="gray"
+          >
+            <strong>WHY HESITATE TO REGISTER?</strong>
+          </p>
+          <h1>
+            Learn At Your Own
+            <span>Pace</span>
+          </h1>
+          <div class="vantaggi">
+            <p class="gray">
+              <font-awesome-icon
+                class="green"
+                icon="fa-solid fa-check"
+              />
+              Select & customize courses to your preferences
             </p>
+            <p class="gray">
+              <font-awesome-icon
+                class="green"
+                icon="fa-solid fa-check"
+              />
+              Change the tutor and make arrangements
+            </p>
+            <p class="gray">
+              <font-awesome-icon
+                class="green"
+                icon="fa-solid fa-check"
+              />
+              Partecipate in event to join others
+            </p>
+            <p class="gray">
+              <font-awesome-icon
+                class="green"
+                icon="fa-solid fa-check"
+              />
+              Get the desired certificate delivered at house
+            </p>
+          </div>
+          <button>Get started for free</button>
+        </div>
+        <div class="img-section">
+          <img
+            src="images\home-5-image-01-269x300.png"
+            alt="Pace-img"
+            class="jumbo"
+          >
+          <img
+            src="images\maxcoach-shape-05-100x100.png"
+            alt="smaller"
+            class="small"
+          >
+        </div>
+      </div>
+      <div class="second-container">
+        <div class="img-section">
+          <img
+            src="images\home-5-image-02.png"
+            alt="Pace-img"
+            class="jumbo"
+          >
+          <img
+            src="images\maxcoach-shape-07-150x150.png"
+            alt="smaller"
+            class="small-one"
+          >
+          <img
+            src="images\maxcoach-shape-01.png"
+            alt="smaller"
+            class="small-two"
+          >
+          <img
+            src="images\maxcoach-shape-02.png"
+            alt="smaller"
+            class="small-tre"
+          >
+        </div>
+        <div class="text">
+          <p class="gray">
+            WHAT ARE YOU LOOKING FOR?
+          </p>
+          <h1>Learn on your <span>mobile</span> anytime!</h1>
+          <p class="text-part">
+            Save lessons for later and ravisa or write notes at any
+            time and on multiple devices on your way. Never
+            interfere with activities of enjoyment or other precious moments.
+          </p>
+          <button>Download Our App</button>
+        </div>
+      </div>
+    </div>
+    <div class="fifth-section">
+      <div class="container">
+        <div class="title">
+          <p>GET IN CONTACT NOW</p>
+          <h1>Upcoming <span>Events</span></h1>
+        </div>
+        <div class="event-container">
+          <div
+            v-for="objEvent in events"
+            :key="objEvent.i"
+          >
+            <div class="text">
+              <p>
+                <font-awesome-icon icon="fa-solid fa-location-dot" />
+                {{ objEvent.position }}
+              </p>
+              <h3>{{ objEvent.event }}</h3>
+            </div>
+            <div class="date-container">
+              <p>{{ objEvent.day }}</p>
+              <p>{{ objEvent.mounth }}</p>
+              <button>Get ticket</button>
+            </div>
           </div>
         </div>
       </div>
@@ -243,6 +371,44 @@ export default {
           students: '76',
         },
 
+      ],
+      events: [
+        {
+          position: 'Texas, US',
+          event: 'Storytelling Workshop',
+          day: '22',
+          mounth: 'NOV',
+        },
+        {
+          position: 'New York, US',
+          event: 'Painting Art Contest 2020',
+          day: '10',
+          mounth: 'OCT',
+        },
+        {
+          position: 'Hamburg, Germany',
+          event: 'International Art Fair 2020',
+          day: '23',
+          mounth: 'NOV',
+        },
+        {
+          position: 'Illinois, US',
+          event: 'Street Performance: Call for Artist',
+          day: '15',
+          mounth: 'DEC',
+        },
+        {
+          position: 'Illinois, US',
+          event: 'Consumer Food Safety Education Conference',
+          day: '22',
+          mounth: 'JUL',
+        },
+        {
+          position: 'Dubai',
+          event: 'How meditation improve your mental health',
+          day: '12',
+          mounth: 'AUG',
+        },
       ],
     };
   },
@@ -388,8 +554,205 @@ export default {
     .card{
       flex: 0 0 30%;
       margin: 2rem 0;
+      position: relative;
+      border-radius: 5px;
+        &:hover{
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5), 0 6px 20px 0 rgba(0, 0, 0, 0.3);
+          background-color: #F5F5F5;
+        }
+      .gray{
+        color: gray;
+      }
+      .right{
+        margin-left: 2rem;
+      }
+      .price{
+        color: white;
+        background-color: #35AE96;
+        position: absolute;
+        top: 0;
+        right: 0;
+        padding: .5rem .7rem;
+        border-radius: 0 5px;
+      }
       .poster{
         width: 100%;
+        border-radius: 5px 5px 0 0;
+      }
+      .card-text{
+        padding:1rem 1.5rem 1.5rem 1.5rem;
+        line-height: 2rem;
+        .teacher-container{
+          display: flex;
+          align-items: center;
+          gap: 1rem;
+          img{
+            height: 35px;
+            border-radius: 100%;
+          }
+        }
+        h2{
+          margin-top: .7rem;
+        }
+        .contenuto{
+          margin-top: 1.3rem;
+        }
+      }
+
+    }
+  }
+  .button-container{
+    text-align: center;
+    button{
+      background-color: #35AE96;
+      color: white;
+      border: 0px;
+      padding: 1.3rem 4rem;
+      border-radius: 5px;
+    }
+  }
+}
+.fourth-section{
+  margin-bottom: 5rem;
+  .first-container{
+    max-width: 1200px;
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .text{
+      flex: 0 0 30%;
+      .gray{
+        color: gray;
+      }
+      .green{
+        color: #35AE96;
+        margin-right: .7rem;
+      }
+      h1{
+        font-size: 50px;
+        display: inline-block;
+        margin-right: 1rem;
+        color: #3F3A64;
+        span{
+        font-size: 50px;
+        color: #35AE96;
+        font-weight: normal;
+        }
+      }
+      .vantaggi{
+        line-height: 2rem;
+        margin-top: 1rem;
+      }
+      button{
+      background-color: #35AE96;
+      color: white;
+      border: 0px;
+      padding: 1rem 2rem;
+      border-radius: 5px;
+      margin-top: 2rem;
+      }
+    }
+    .img-section{
+      flex: 0 0 50%;
+      background-image: url(../../public/images/underlay-shape-grey.svg);
+      background-repeat: no-repeat;
+      background-size: 400px;
+      padding: 2rem 0 5rem 5rem;
+      background-position-x: 0px;
+      background-position-y: 160px;
+      position: relative;
+      .jumbo{
+        height: 500px;
+        margin: auto;
+      }
+      .small{
+        position: absolute;
+        height: 140px;
+        top: 300px;
+        right: 60px;
+      }
+    }
+  }
+  .second-container{
+    max-width: 1200px;
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    margin-top: 3rem;
+    .img-section{
+      flex: 0 0 50%;
+      background-image: url(../../public/images/underlay-shape-grey.svg);
+      background-repeat: no-repeat;
+      background-size: 400px;
+      padding: 2rem 0 5rem 5rem;
+      background-position-x: 0px;
+      background-position-y: 160px;
+      position: relative;
+      .jumbo{
+        height: 500px;
+        margin: auto;
+      }
+      .small-one{
+        z-index: -1;
+        position: absolute;
+        right: 60px;
+        top: 330px;
+      }
+      .small-two{
+        position: absolute;
+        top: 290px;
+        right: 550px;
+      }
+      .small-tre{
+        position: absolute;
+        height: 80px;
+        top: 20px;
+        left:480px;
+      }
+    }
+    .text{
+      flex: 0 0 40%;
+      .gray{
+        color: gray;
+      }
+      h1{
+        font-size: 50px;
+        color: #3F3A64;
+        margin: 2rem 0;
+        span{
+          color: #35AE96;
+          font-weight: normal;
+        }
+      }
+      .text-part{
+        margin-bottom: 3.5rem;
+        line-height: 1.5rem;
+      }
+      button{
+      background-color: #35AE96;
+      color: white;
+      border: 0px;
+      padding: 1rem 2rem;
+      border-radius: 5px;
+    }
+    }
+  }
+
+}
+.fifth-section{
+  padding-bottom: 4rem;
+  .title{
+    text-align: center;
+    p{
+      color: gray;
+    }
+    h1{
+      color: #3F3A64;
+      span{
+        color: #35AE96;
+        font-weight: normal;
       }
     }
   }
