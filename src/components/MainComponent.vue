a1273ea4c206d0888ecbd0c7b134acbe49f8b289
MicheleRai <rainonemichele1999@gmail.com>
2022-12-06 12:55:12 +0100

    dropdown fixed

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -41,17 +41,25 @@
         <h1>Career Prospective</h1>
         <div class="dropdown">
           <div
-            v-for="(objProspettiva, i) in prospettive"
+            v-for="(objProspettiva) in prospettive"
             :key="(objProspettiva.i)"
+            class="obj"
           >
             <p
               class="title"
-              @click="trigger(i)"
-              @keypress="trigger(i)"
+              :class="{green:objProspettiva.toggle}"
+              @click="trigger(objProspettiva)"
+              @keypress="trigger(objProspettiva)"
             >
               <strong>{{ objProspettiva.title }}</strong>
-              <font-awesome-icon icon="fa-solid fa-circle-plus" />
-              <font-awesome-icon icon="fa-solid fa-circle-minus" />
+              <font-awesome-icon
+                :class="{hide:objProspettiva.toggle}"
+                icon="fa-solid fa-circle-plus"
+              />
+              <font-awesome-icon
+                :class="{hide:!objProspettiva.toggle}"
+                icon="fa-solid fa-circle-minus"
+              />
             </p>
             <p v-show="objProspettiva.toggle">
               {{ objProspettiva.text }}
@@ -93,25 +101,26 @@ export default {
       prospettive: [
         {
           title: 'Multiple Platforms Supported for Teaching & Studyng',
-          text: 'MaxCoach support the act of teaching and learning on multiple plattforms like online and ofline via material downolads. We know things aren?t supposed to be devoured in a short time, you can always acces our knowledge base from any device.',
+          text: 'MaxCoach support the act of teaching and learning on multiple plattforms like online and ofline via material downolads. We know things aren\'t supposed to be devoured in a short time, you can always acces our knowledge base from any device.',
           toggle: true,
         },
         {
           title: 'Multiple Course Partecipation at the Same Time',
-          text: 'MaxCoach support the act of teaching and learning on multiple plattforms like online and ofline via material downolads. We know things aren?t supposed to be devoured in a short time, you can always acces our knowledge base from any device.',
+          text: 'MaxCoach support the act of teaching and learning on multiple plattforms like online and ofline via material downolads. We know things aren\'t supposed to be devoured in a short time, you can always acces our knowledge base from any device.',
           toggle: false,
         },
         {
           title: 'Track Study Progress & Deliver Prompt Feedback',
-          text: 'MaxCoach support the act of teaching and learning on multiple plattforms like online and ofline via material downolads. We know things aren?t supposed to be devoured in a short time, you can always acces our knowledge base from any device.',
+          text: 'MaxCoach support the act of teaching and learning on multiple plattforms like online and ofline via material downolads. We know things aren\'t supposed to be devoured in a short time, you can always acces our knowledge base from any device.',
           toggle: false,
         },
       ],
     };
   },
   methods: {
-    trigger(i) {
-      this.objProspettiva[i].toggle = !this.objProspettiva[i].toggle;
+    trigger(objProspettiva) {
+      const obj = objProspettiva;
+      obj.toggle = !obj.toggle;
     },
   },
 };
@@ -155,6 +164,14 @@ export default {
   margin-bottom: 5rem;
   .second-sec-image{
     flex: 0 0 45%;
+    background-image: url(../../public/images/underlay-shape-lilla.svg);
+    background-size: 400px;
+    background-repeat: no-repeat;
+    background-position: left bottom ;
+    img{
+    width: 60%;
+    margin-left: 10rem;
+    }
   }
   .second-sec-text{
     flex: 0 0 45%;
@@ -173,13 +190,23 @@ export default {
       font-size: 45px;
     }
     .dropdown{
-      p{
-        display: flex;
-        justify-content: space-between;
-        background-color: #62C5B5;
-        color: white;
-        padding: 1rem .7rem;
-        border-radius: 5px;
+      .obj{
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+      border-radius: 5px;
+      margin: 2rem 0;
+        .green{
+          background-color: #62C5B5;
+          color: white;
+        }
+        .hide{
+          display: none;
+        }
+        p{
+          display: flex;
+          justify-content: space-between;
+          padding: 1rem .7rem;
+          border-radius: 5px;
+      }
       }
     }
   }
